35b4511858a2990bc463bca759f5d37f63f63437
Mohamed Nur <mohamed.nur@cds-snc.ca>
2022-04-21 11:19:52 -0400

    chore: remove unused service registry

--- terragrunt/aws/sso_proxy/service_discovery.tf
+++ terragrunt/aws/sso_proxy/service_discovery.tf
@@ -22,22 +22,3 @@ resource "aws_service_discovery_service" "auth" {
     failure_threshold = 5
   }
 }
-
-resource "aws_service_discovery_service" "httpd" {
-  name = "httpd"
-
-  dns_config {
-    namespace_id = aws_service_discovery_private_dns_namespace.internal.id
-
-    dns_records {
-      ttl  = 10
-      type = "A"
-    }
-
-    routing_policy = "MULTIVALUE"
-  }
-
-  health_check_custom_config {
-    failure_threshold = 5
-  }
-}
