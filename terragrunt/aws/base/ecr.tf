610f727514b24daab0be2cf38b4523f7cd8fe95d
Mohamed Nur <mohamed.nur@cds-snc.ca>
2022-04-25 15:37:14 -0400

    fix: missed checkov items

--- terragrunt/aws/base/ecr.tf
+++ terragrunt/aws/base/ecr.tf
@@ -5,6 +5,10 @@ resource "aws_ecr_repository" "cartography" {
   name                 = "ecs/cloud_asset_inventory/cartography"
   image_tag_mutability = "MUTABLE"
 
+  encryption_configuration {
+    encryption_type = "KMS"
+  }
+
   image_scanning_configuration {
     scan_on_push = true
   }
@@ -13,6 +17,10 @@ resource "aws_ecr_repository" "cartography" {
 resource "aws_ecr_repository" "neo4j_ingestor" {
   name                 = "ecs/cloud_asset_inventory/neo4j_ingestor"
   image_tag_mutability = "MUTABLE"
+
+  encryption_configuration {
+    encryption_type = "KMS"
+  }
 
   image_scanning_configuration {
     scan_on_push = true
