9fc47fedaacf5637e991de1b5b0a40f0d74156c6
Mohamed Nur <mohamed.nur@cds-snc.ca>
2022-04-28 10:24:39 -0400

    feat: output lb dns name

--- terragrunt/aws/cloud_asset_inventory/outputs.tf
+++ terragrunt/aws/cloud_asset_inventory/outputs.tf
@@ -2,3 +2,8 @@ output "cloud_asset_inventory_vpc_id" {
   description = "The VPC ID for Cloud Asset Inventory"
   value       = module.vpc.vpc_id
 }
+
+output "cloud_asset_inventory_load_balancer_dns" {
+  description = "The DNS name of the Cloud Asset Inventory load balancer"
+  value       = aws_lb.cartography.dns_name
+}
