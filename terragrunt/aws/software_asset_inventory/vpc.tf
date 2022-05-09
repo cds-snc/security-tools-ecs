1ef64b1db7d6cdda2afae367c12be14d7de2f982
Mohamed Nur <mohamed.nur@cds-snc.ca>
2022-05-09 12:12:05 -0400

    feat: grant egress to rds

--- terragrunt/aws/software_asset_inventory/vpc.tf
+++ terragrunt/aws/software_asset_inventory/vpc.tf
@@ -47,6 +47,14 @@ resource "aws_security_group" "dependencytrack" {
   }
 
   egress {
+    description = "Access to RDS Postgresql"
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = module.vpc.private_subnet_cidr_blocks
+  }
+
+  egress {
     description = "Outbound access to internet & elasticsearch"
     from_port   = 443
     to_port     = 443
