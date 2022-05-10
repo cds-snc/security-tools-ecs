a0cacd3eee4b5a9cd039dc47c0be9ee22d4b1e22
Mohamed Nur <85885638+mohamed-cds@users.noreply.github.com>
2022-05-10 08:39:41 -0400

    fix: bump rds instance class to the smallest allowed class (#46)

--- terragrunt/aws/software_asset_inventory/rds.tf
+++ terragrunt/aws/software_asset_inventory/rds.tf
@@ -6,7 +6,7 @@ module "dependencytrack_db" {
   engine         = "aurora-postgresql"
   engine_version = "13.6"
   instances      = 2
-  instance_class = "db.t3.small"
+  instance_class = "db.t3.medium"
   username       = aws_ssm_parameter.dependencytrack_db_user.value
   password       = aws_ssm_parameter.dependencytrack_db_password.value
 
